782a41830344a6543fa2da433639d63c12c2922f
gabrielscotti <gabrielscotti54@gmail.com>
2025-04-14 21:35:14 -0300

    Update TesteApp.java

--- src/TesteApp.java
+++ src/TesteApp.java
@@ -2,6 +2,6 @@ public class TesteApp {
 
     public static void main(String[] args) {
         
-        System.out.println("Teste Git Hub");
+        System.out.println("Teste Git Hub ");
     }
 }
